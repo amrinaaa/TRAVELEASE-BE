a74bd3cfbefff68b08d4101552a63d1551d947fc
amrina <123223063+amrinaaa@users.noreply.github.com>
2025-03-17 00:09:45 +0800

    fix deploy

--- prisma/prisma.client.js
+++ prisma/prisma.client.js
@@ -1,12 +1,5 @@
 import { PrismaClient } from "@prisma/client";
-import { DATABASE_URL } from "../src/utils/env.js";
 
-const prisma = new PrismaClient({
-    datasources: {
-        db: {
-            url: DATABASE_URL,
-        },
-    }
-});
+const prisma = new PrismaClient();
 
 export default prisma;
