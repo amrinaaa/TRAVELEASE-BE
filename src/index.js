ed466aeb69970eedca40cad6054164058f8b1958
glenngladly26 <glenngladlygessong@gmail.com>
2025-03-16 22:31:30 +0800

    add endpoint checking

--- src/index.js
+++ src/index.js
@@ -13,6 +13,13 @@ app.use(cors());
 app.use(bodyParser.json());
 app.use(cookieParser());
 
+app.get("/", (req, res) =>{
+    res.status(200).json({
+        message: "Server is running",
+        data: null,
+    });
+});
+
 app.use("/api", router);
 docs(app);
 
