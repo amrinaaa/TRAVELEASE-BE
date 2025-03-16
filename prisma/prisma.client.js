087cb8f2eabcc87a110009bad5e5ebe285ed5224
glenngladly26 <glenngladlygessong@gmail.com>
2025-03-16 23:35:34 +0800

    fix prisma

--- prisma/prisma.client.js
+++ prisma/prisma.client.js
@@ -1,12 +1,14 @@
 import { PrismaClient } from "@prisma/client";
-import { DATABASE_URL } from "../src/utils/env.js";
+// import { DATABASE_URL } from "../src/utils/env.js";
 
-const prisma = new PrismaClient({
-    datasources: {
-        db: {
-            url: DATABASE_URL,
-        },
-    }
-});
+const prisma = new PrismaClient(
+    // {
+    //     datasources: {
+    //         db: {
+    //             url: DATABASE_URL,
+    //         },
+    //     }
+    // }
+);
 
 export default prisma;
