4581456026d1b7e704d1f4e47ff9ddeccb2f16d8
glenngladly26 <glenngladlygessong@gmail.com>
2025-04-12 14:23:50 +0800

    fix endpoint ketersediaan kursi penerbagan

--- src/services/airport/seat.js
+++ src/services/airport/seat.js
@@ -3,42 +3,40 @@ import prisma from "../../../prisma/prisma.client.js";
 export default {
     async getSeatService (flightId) {
         try {
-            const flight = await prisma.flight.findUnique({
-                where: { id: flightId },
+            const allSeats = await prisma.seat.findMany({
                 select: {
                   id: true,
-                  planeId: true,
-                  plane: {
-                    include: {
-                      seatCategories: {
-                        include: {
-                          seats: true
-                        }
-                      }
-                    }
-                  }
-                }
+                  name: true,
+                  seatCategory: {
+                    select: {
+                      id: true,
+                      name: true,
+                    },
+                  },
+                },
               });
               
-            if (!flight) throw new Error("Flight not found");
-        
-            const bookedTickets = await prisma.ticket.findMany({
-                where: { flightId },
-                select: { seatId: true }
+            const usedTickets = await prisma.ticket.findMany({
+                where: {
+                  flightId: flightId,
+                },
+                select: {
+                  seatId: true,
+                },
             });
-        
-            const bookedSeatIds = new Set(bookedTickets.map(ticket => ticket.seatId));
-        
-            const allSeats = flight.plane.seatCategories.flatMap(sc =>
-                sc.seats.map(seat => ({
-                  seatId: seat.id,
-                  seatName: seat.name,
-                  seatCategory: sc.name,
-                  available: !bookedSeatIds.has(seat.id)
-                }))
-            );
-        
-            return allSeats;
+              
+            const usedSeatIds = new Set(usedTickets.map(ticket => ticket.seatId));
+              
+            const result = allSeats.map(seat => ({
+                id: seat.id,
+                name: seat.name,
+                seatCategory: {
+                  name: seat.seatCategory.name,
+                },
+                isAvailable: !usedSeatIds.has(seat.id),
+            }));
+            
+            return result;
         }catch (error) {
             throw new Error(error.messasge);
         };
