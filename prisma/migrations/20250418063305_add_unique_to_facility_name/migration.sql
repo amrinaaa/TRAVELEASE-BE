b0cbcc414942fccd06409f42a2e612cb07181043
glenngladly26 <glenngladlygessong@gmail.com>
2025-04-19 16:52:15 +0800

    feat: reset migrations

--- prisma/migrations/20250418063305_add_unique_to_facility_name/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - A unique constraint covering the columns `[facilityName]` on the table `Facility` will be added. If there are existing duplicate values, this will fail.
-
-*/
--- CreateIndex
-CREATE UNIQUE INDEX "Facility_facilityName_key" ON "Facility"("facilityName");
